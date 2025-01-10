cc4f807ac085e3f6b764483eef1df17ba5e4a349
felipedev13092005 <juanfelipevalbuenavalderrama4@gmail.com>
2025-01-10 12:48:54 -0500

    ruta de traer productos

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Config;
 // 	imagen: string | null // inv_catalogo
 // 	cuentaVenta: string // inv_parametros.cta_venta
 // 	ivaventa: string // inv_parametros.iva2
-// 	vrUnitario: string // inv_prod2024 se obtiene de dividiendo san(mes) entre can(mes)
+// 	vrUnitario: string // inv_prod2024 se obtiene de dividiendo san(men) entre can(mes)
 // 	marca: string // inv_catalogo
 // 	linea: string // inv_catalogo
 // 	bodega: string // operacion.parametros_inv se filtra por id_empresa
@@ -28,56 +28,164 @@ use Illuminate\Support\Facades\Config;
 // 	cod_barras: string // inv_catalogo
 // 	estado: string // inv_catalogo
 // }
+// export interface ProductWithAmount extends Product {
+// 	amount: number
+// }
 
 class ProductosController extends Controller
 {
     public function index(Request $request)
     {
+        $precio_fk = $request->query('precio_fk');
         try {
             $year = date('Y');
             $month = date('m');
             // convertir el mes a un número de 1 a 12
             $month = (int)$month;
-            $jwt_payload = $request->attributes->get('jwt_payload');
-            // cortar los dos primeros caracteres del db de jwt_payload
-            $dbId = (int)substr($jwt_payload['db'], 2);
-            $sqlObtenerProducto = "SELECT codigo, nombre, descuento, iva, referencia, imagen, marca_FK, linea_FK, id_invcatalogo, cod_barras, estado, inv_prod" . $year . ".can" . $month . " as stock, inv_prod" . $year . ".san" . $month . " as vrTotal FROM inv_catalogo
-                                   JOIN inv_prod" . $year . " ON inv_catalogo.id_invcatalogo = inv_prod" . $year . ".id_catalogo;";
-            $productos = Db::connection('dynamic_database')->select($sqlObtenerProducto);
-            $productos = collect($productos);
-            Config::set('database.connections.dynamic_database', [
-                'driver' => 'mysql',
-                'host' => env('DB_HOST', '127.0.0.1'),
-                'port' => env('DB_PORT', '3306'),
-                'database' => "lempapco_operacion",
-                'username' => env('DB_USERNAME', 'root'),
-                'password' => env('DB_PASSWORD', ''),
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-            ]);
-            $unidad = DB::reconnect('dynamic_database')->select("SELECT * FROM parametros_inv WHERE id_empresa = $dbId");
-            $productosMap = $productos->map(function ($product) {
-                return [
-                    'codigo' => $product->codigo,
-                    'descripcion' => $product->nombre,
-                    'descuento' => $product->descuento,
-                    'iva' => $product->iva,
-                    'referencia' => $product->referencia,
-                    'imagen' => $product->imagen,
-                    'marca' => $product->marca_FK,
-                    'linea' => $product->linea_FK,
-                    'invCatalogo' => $product->id_invcatalogo,
-                    'nombre' => $product->nombre,
-                    'cod_barras' => $product->cod_barras,
-                    'estado' => $product->estado,
-                    'stock' => $product->stock,
-                    'vrUnitario' => $product->stock > 0 ? $product->vrTotal / $product->stock : 0,
-                ];
-            });
-            return response()->json([
-                'unidad' => $unidad,
-                'productos' => $productosMap,
-            ]);
+            // si se recibe un precio_fk pero no es un número
+            if ($precio_fk && !is_numeric($precio_fk)) {
+                return response()->json([
+                    'message' => 'El precio_fk debe ser un número',
+                ], 400);
+            }
+
+            // si el precio_fk es menor a 1 o mayor a 6
+            if (($precio_fk && !is_numeric($precio_fk)) && ($precio_fk < 1 || $precio_fk > 6)) {
+                return response()->json([
+                    'message' => 'El precio_fk debe ser un número entre 1 y 6',
+                ], 400);
+            }
+            if ($precio_fk === '0') {
+                return response()->json([
+                    'message' => 'El precio_fk debe ser un número entre 1 y 6',
+                ], 400);
+            }
+            if (!$precio_fk) {
+                // si se manda el precio fk
+                $sqlObtenerProducto = "SELECT codigo, nombre, descuento, iva, referencia, imagen, unidad_FK, marca_FK, linea_FK, id_invcatalogo, cod_barras, estado, vr_unitario, inv_prod" . $year . ".can" . $month . " as stock FROM inv_catalogo
+                                       JOIN inv_prod" . $year . " ON inv_catalogo.id_invcatalogo = inv_prod" . $year . ".id_catalogo;";
+                $productos = Db::connection('dynamic_database')->select($sqlObtenerProducto);
+                $productos = collect($productos);
+                Config::set('database.connections.dynamic_database', [
+                    'driver' => 'mysql',
+                    'host' => env('DB_HOST', '127.0.0.1'),
+                    'port' => env('DB_PORT', '3306'),
+                    'database' => "lempapco_operacion",
+                    'username' => env('DB_USERNAME', 'root'),
+                    'password' => env('DB_PASSWORD', ''),
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                ]);
+                $unidades = DB::reconnect('dynamic_database')->select("SELECT codigo, descripcion FROM parametros_inv WHERE concepto='Medida'");
+                $unidades = collect($unidades); // Ya es una colección
+
+                $productosMap = $productos->map(function ($product) use ($unidades) {
+                    $unidad = $product->unidad_FK ?? '';
+                    if ($unidad) {
+                        $unidad = $unidades->first(function ($u) use ($unidad) {
+                            return $u->codigo === $unidad;
+                        });
+                        $unidad = $unidad ? $unidad->descripcion : '';
+                    }
+                    return [
+                        'codigo' => $product->codigo,
+                        'descripcion' => $product->nombre,
+                        'descuento' => $product->descuento,
+                        'iva' => $product->iva,
+                        'referencia' => $product->referencia,
+                        'imagen' => $product->imagen ? "https://lempap.net.co/" . $product->imagen : null,
+                        'marca' => $product->marca_FK,
+                        'linea' => $product->linea_FK,
+                        'invCatalogo' => $product->id_invcatalogo,
+                        'nombre' => $product->nombre,
+                        'cod_barras' => $product->cod_barras,
+                        'estado' => $product->estado,
+                        'vr_unitario' => $product->vr_unitario,
+                        'stock' => $product->stock,
+                        'unidad' => $unidad,
+                    ];
+                });
+
+                return response()->json([
+                    // 'unidad' => $unidades,
+                    'productos' => $productosMap,
+                ]);
+            } else {
+                // si se manda el precio fk
+                $sqlObtenerProducto = "
+                                        SELECT 
+                                        inv_catalogo.codigo, 
+                                        inv_catalogo.nombre, 
+                                        inv_catalogo.descuento, 
+                                        inv_catalogo.iva, 
+                                        inv_catalogo.referencia, 
+                                        inv_catalogo.imagen, 
+                                        inv_catalogo.unidad_FK, 
+                                        inv_catalogo.marca_FK, 
+                                        inv_catalogo.linea_FK, 
+                                        inv_catalogo.id_invcatalogo, 
+                                        inv_catalogo.cod_barras, 
+                                        inv_catalogo.estado, 
+                                        inv_prod" . $year . ".can" . $month . " AS stock, 
+                                        inv_lprecios.vr_" . $precio_fk . " AS vr_unitario
+                                    FROM 
+                                        inv_catalogo 
+                                    JOIN 
+                                        inv_prod" . $year . " 
+                                    ON 
+                                        inv_catalogo.id_invcatalogo = inv_prod" . $year . ".id_catalogo 
+                                    JOIN 
+                                        inv_lprecios 
+                                    ON 
+                                        inv_catalogo.id_invcatalogo = inv_lprecios.id_catalogo;
+                ";
+                $productos = Db::connection('dynamic_database')->select($sqlObtenerProducto);
+                $productos = collect($productos);
+                Config::set('database.connections.dynamic_database', [
+                    'driver' => 'mysql',
+                    'host' => env('DB_HOST', '127.0.0.1'),
+                    'port' => env('DB_PORT', '3306'),
+                    'database' => "lempapco_operacion",
+                    'username' => env('DB_USERNAME', 'root'),
+                    'password' => env('DB_PASSWORD', ''),
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                ]);
+                $unidades = DB::reconnect('dynamic_database')->select("SELECT codigo, descripcion FROM parametros_inv WHERE concepto='Medida'");
+                $unidades = collect($unidades); // Ya es una colección
+
+                $productosMap = $productos->map(function ($product) use ($unidades) {
+                    $unidad = $product->unidad_FK ?? '';
+                    if ($unidad) {
+                        $unidad = $unidades->first(function ($u) use ($unidad) {
+                            return $u->codigo === $unidad;
+                        });
+                        $unidad = $unidad ? $unidad->descripcion : '';
+                    }
+                    return [
+                        'codigo' => $product->codigo,
+                        'descripcion' => $product->nombre,
+                        'descuento' => $product->descuento,
+                        'iva' => $product->iva,
+                        'referencia' => $product->referencia,
+                        'imagen' => $product->imagen ? "https://lempap.net.co/" . $product->imagen : null,
+                        'marca' => $product->marca_FK,
+                        'linea' => $product->linea_FK,
+                        'invCatalogo' => $product->id_invcatalogo,
+                        'nombre' => $product->nombre,
+                        'cod_barras' => $product->cod_barras,
+                        'estado' => $product->estado,
+                        'vr_unitario' => $product->vr_unitario,
+                        'stock' => $product->stock,
+                        'unidad' => $unidad,
+                    ];
+                });
+
+                return response()->json([
+                    // 'unidad' => $unidades,
+                    'productos' => $productosMap,
+                ]);
+            }
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'Error querying the database',
